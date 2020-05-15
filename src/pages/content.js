6fa5f122143a267422fda88ecf02220c80e8f105
Jeraldo <41482782+Jerald-o@users.noreply.github.com>
2020-05-14 21:02:41 -0400

    Content Page Col Layout

---
+++ src/pages/content.js
@@ -0,0 +1,50 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import "../components/layout.css"
+import { Row, Col, Card, Container } from "react-bootstrap";
+
+
+const ContentPage = () => (
+  <Layout>
+    <SEO title="Content" />
+      <div className="Hero-Content">
+        <Container>
+          <Row className="content-card-row">
+            <Col sm={2} md={5} lg={8} className="content-card">
+                {/*these are props need to be created*/}
+                {/*articleTitle,  articleBG , articleAuthor  articleTime ,  articleAuthor, authorIMG*/}
+
+    
+  
+            </Col>
+
+            <Col sm={1} md={2} lg={4} className="content-card-side">
+                
+            </Col>
+
+
+
+        </Row>
+
+        <Row>
+            <Col className="content-card">
+                
+            </Col>
+
+            <Col className="content-card" >
+                
+            </Col>
+
+            <Col className="content-card">
+                
+            </Col>
+         </Row>
+
+        </Container>
+      </div>
+  </Layout>
+)
+
+export default ContentPage
