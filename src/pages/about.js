6ee955b206431cfa665b94d2fbec907162702785
Shain Dholakiya <shainshain99@gmail.com>
2020-05-17 21:05:00 -0400

    updates

--- src/pages/about.js
+++ src/pages/about.js
@@ -12,18 +12,18 @@ const AboutUsPage = () => (
     <SEO title="Home" />
       <div className="Hero-AboutUs Hero">
         <Row>
-          <Col sm={9} md={7} lg={5}>
+          <Col sm={12} md={7} lg={5}>
             <div className="HeroGroup">
               <h1>About Us</h1>
-              <p>We do a whole lot of cool stuff. But this is really just a paragraph to simulate what our real about us would look like.</p>
+              <p className="description">We do a whole lot of cool stuff. But this is really just a paragraph to simulate what our real about us would look like.</p>
               <a target="_blank" href="https://www.instagram.com/enigsu/?hl=en"><FontAwesomeIcon icon={faInstagram} size="4x"/></a>
               <a target="_blank" href="https://twitter.com/blockchaingsu"><FontAwesomeIcon icon={faYoutube} size="4x"/></a>
               <a target="_blank" href="https://twitter.com/blockchaingsu"><FontAwesomeIcon icon={faTwitter} size="4x"/></a>
             </div>
           </Col>
-          <Col sm={3} md={5} lg={{span: 6, offset: 1}}>
+          <Col sm={12} md={12} lg={{span: 6, offset: 1}}>
               <Row>
-                <Col sm={6} md={7} lg={8}>
+                <Col sm={10} md={10} lg={8}>
                     <div className="about-us-content">
                         <h2>Tariq Waseem</h2>
                         <h3>BCGSU</h3>
@@ -32,18 +32,22 @@ const AboutUsPage = () => (
                 </Col>
               </Row>
               <Row>
-                <div className="about-us-content">
-                    <h2>Ryan Sims</h2>
-                    <h3>BCGSU</h3>
-                    <p>This guy is also cool as heck reall chill guy.</p>
-                </div>
+                <Col sm={10} md={10} lg={8}>
+                  <div className="about-us-content">
+                      <h2>Ryan Sims</h2>
+                      <h3>BCGSU</h3>
+                      <p>This guy is also cool as heck reall chill guy.</p>
+                  </div>
+                </Col>
               </Row>
               <Row>
-                <div className="about-us-content">
-                    <h2>Deralyn Swiley</h2>
-                    <h3>BCGSU</h3>
-                    <p>This gal is cool as heck reall awesome chill gal.</p>
-                </div>
+                <Col sm={10} md={10} lg={8}>
+                  <div className="about-us-content">
+                      <h2>Deralyn Swiley</h2>
+                      <h3>BCGSU</h3>
+                      <p>This gal is cool as heck reall awesome chill gal.</p>
+                  </div>
+                </Col>
               </Row>
           </Col>
         </Row>
--- src/pages/resources.js
+++ src/pages/resources.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Link } from "gatsby"
+// import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -8,61 +8,61 @@ import { Row, Col, Card, Container } from "react-bootstrap";
 
 import hackernoon from "../images/hackernoon-icon.jpeg"
 import medium from "../images/medium-icon.png"
-import youtube from "../images/youtube-icon.jpg"
-import reddit from "../images/reddit-icon.png"
-import coindesk from "../images/coindesk-icon.png"
+// import youtube from "../images/youtube-icon.jpg"
+// import reddit from "../images/reddit-icon.png"
+// import coindesk from "../images/coindesk-icon.png"
 
 const ResourcesPage = () => (
   <Layout>
     <SEO title="Home" />
       <div className="Hero Hero-Resources">
         <Row>
-          <Col sm={{span: 7, offset: 5}} md={{span: 7, offset: 5}} lg={{span: 6, offset: 6}}>
+          <Col sm={{span: 8, ofsset: 4}} md={{span: 7, offset: 5}} lg={{span: 6, offset: 6}}>
             <div className="Resources">
               <h1><u>Blockchain Resources</u></h1>
             </div>
           </Col>
         </Row>
         <Container>
-          <Row className="resource-icon-row">
-            <Col>
+          <Row>
+            <Col sm={6} md={6} lg={4}>
                 <a target="_blank" href="https://hackernoon.com/">
                     <Card className="resource-icons">
-                        <Card.Img className="resource-icon-img" variant="top" src={hackernoon} />
+                        <Card.Img variant="top" src={hackernoon} />
                     </Card>
                 </a>
             </Col>
-            <Col>
+            <Col sm={6} md={6} lg={4}>
                 <a target="_blank" href="https://medium.com/">
                     <Card className="resource-icons">
                         <Card.Img variant="top" src={medium} />
                     </Card>
                 </a>
             </Col>
-            <Col>
+            <Col sm={6} md={6} lg={4}>
                 <a target="_blank" href="https://hackernoon.com/">
                     <Card className="resource-icons">
                         <Card.Img variant="top" src={hackernoon} />
                     </Card>
                 </a>
             </Col>
-            </Row>
-            <Row>
-            <Col>
+            {/* </Row>
+            <Row> */}
+            <Col sm={6} md={6} lg={4}>
                 <a target="_blank" href="https://www.reddit.com/">
                     <Card className="resource-icons">
-                        <Card.Img variant="top" src={reddit} />
+                        <Card.Img variant="top" src={medium} />
                     </Card>
                 </a>
             </Col>
-            <Col>
+            <Col sm={6} md={6} lg={4}>
                 <a target="_blank" href="https://www.coindesk.com/">
                     <Card className="resource-icons">
                         <Card.Img variant="top" src={hackernoon} />
                     </Card>
                 </a>
             </Col>
-            <Col>
+            <Col sm={6} md={6} lg={4}>
                 <a target="_blank" href="https://medium.com/">
                     <Card className="resource-icons">
                         <Card.Img variant="top" src={medium} />
