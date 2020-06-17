e8cdc3da4916f5622175ccd9f46361fa64958f03
Shain Dholakiya <shainshain99@gmail.com>
2020-06-17 17:03:12 -0400

    content page

--- src/pages/content.js
+++ src/pages/content.js
@@ -3,99 +3,76 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import "../components/layout.css"
-import { Row, Col, Card, Container,} from "react-bootstrap";
+import { Row, Col, Card, Container,} from "react-bootstrap"
+
+import ArticlePic1 from "../images/ArticleBG.png"
+// import ArticlePic2 from "../images/ArticleBG-Main.png"
 
 
 const ContentPage = () => (
   <Layout>
     <SEO title="Content" />
-      <div className="Hero-Content">
-      <h1 className="content-header">Latest Content</h1>
+      <div className="Hero Hero-Resources">
+        <Row>
+          <Col sm={{span: 8}} md={{span: 7}} lg={{span: 6}}>
+            <div className="Resources">
+              <h1><u>Latest Content</u></h1>
+            </div>
+          </Col>
+        </Row>
         <Container >
-        {/* H1 Needs more padding at the top*/}
           <Row className="content-card-row">
-            
-            <Col className="content_card-main">
-            {/*these are props need to be created*/}
-            {/*articleTitle,  articleBG , articleAuthor  articleTime ,  articleAuthor, authorIMG*/}
-                {/*<Card bg-dark text-white>
-                    <Card.Img variant="top" src="../images/ArticleBG-Main.png" />
-                    <Card.Body>
-                    <Card.Title>Article Title</Card.Title>
-                    <Card.Text>
-                    Some quick example text to build on the card title and make up the bulk of
-                    the card's content.
-                    </Card.Text>
-                    </Card.Body>
-                </Card>
-                */}
-                <Card className="bg-dark text-white">
-                    <Card.Img src="../images/ArticleBG" alt="Card image" />
-                        <Card.ImgOverlay>
-                            <Card.Title>Article Title</Card.Title>
-                            <Card.Text>
-                            Article Author
-                            </Card.Text>
-                            <Card.Text>Article time</Card.Text>
-                        </Card.ImgOverlay>
+            <Col xs={12} sm={12} md={12} lg={6}>
+                <Card className="bg-dark article-icons">
+                    <Card.Img src={ArticlePic1} alt="Card image" />
+                    <Card.ImgOverlay>
+                        <Card.Title>Article Title</Card.Title>
+                        <Card.Text>Article Author</Card.Text>
+                        <Card.Text>Article time</Card.Text>
+                    </Card.ImgOverlay>
                 </Card>
             </Col>
-            <Col className="content_card-side">
-                <Card className="bg-dark text-white">
-                        <Card.Img src="../images/ArticleBG" alt="Card image" />
-                        <Card.ImgOverlay>
-                                <Card.Title>Article Title</Card.Title>
-                                <Card.Text>
-                                Article Author
-                                </Card.Text>
-                                <Card.Text>Article time</Card.Text>
-                        </Card.ImgOverlay>
+            <Col xs={12} sm={12} md={12} lg={6}>
+                <Card className="bg-dark article-icons">
+                    <Card.Img src={ArticlePic1} alt="Card image" />
+                    <Card.ImgOverlay>
+                        <Card.Title>Article Title</Card.Title>
+                        <Card.Text>Article Author</Card.Text>
+                        <Card.Text>Article time</Card.Text>
+                    </Card.ImgOverlay>
                 </Card>
             </Col>
-          </Row>
- 
-          <Row className="content-card-row">
-            <Col className="content-card">
-            <Card className="bg-dark text-white">
-                    <Card.Img src="../images/ArticleBG" alt="Card image" />
-                        <Card.ImgOverlay>
-                            <Card.Title>Article Title</Card.Title>
-                            <Card.Text>
-                            Article Author
-                            </Card.Text>
-                            <Card.Text>Article time</Card.Text>
-                        </Card.ImgOverlay>
-                </Card>
-    
-            </Col>
-
-            <Col className="content-card" >
-            <Card className="bg-dark text-white">
-                    <Card.Img src="../images/ArticleBG" alt="Card image" />
-                        <Card.ImgOverlay>
-                            <Card.Title>Article Title</Card.Title>
-                            <Card.Text>
-                            Article Author
-                            </Card.Text>
-                            <Card.Text>Articlp time</Card.Text>
-                        </Card.ImgOverlay>
+            <Col xs={12} sm={12} md={6} lg={4}>
+                <Card className="bg-dark article-icons">
+                    <Card.Img src={ArticlePic1} alt="Card image" />
+                    <Card.ImgOverlay>
+                        <Card.Title>Article Title</Card.Title>
+                        <Card.Text>Article Author</Card.Text>
+                        <Card.Text>Article time</Card.Text>
+                    </Card.ImgOverlay>
                 </Card>
             </Col>
-
-            <Col className="content-card">
-            <Card className="bg-dark text-white">
-                    <Card.Img src="../images/ArticleBG" alt="Card image" />
-                        <Card.ImgOverlay>
-                            <Card.Title>Article Title</Card.Title>
-                            <Card.Text>
-                            Article Author
-                            </Card.Text>
-                            <Card.Text>Article time</Card.Text>
-                        </Card.ImgOverlay>
+            <Col xs={12} sm={12} md={6} lg={4}>
+                <Card className="bg-dark article-icons">
+                    <Card.Img src={ArticlePic1} alt="Card image" />
+                    <Card.ImgOverlay>
+                        <Card.Title>Article Title</Card.Title>
+                        <Card.Text>Article Author</Card.Text>
+                        <Card.Text>Articlp time</Card.Text>
+                    </Card.ImgOverlay>
+                </Card>
+            </Col>
+            <Col xs={12} sm={12} md={6} lg={4}>
+                <Card className="bg-dark article-icons">
+                    <Card.Img src={ArticlePic1} alt="Card image" />
+                    <Card.ImgOverlay>
+                        <Card.Title>Article Title</Card.Title>
+                        <Card.Text>Article Author</Card.Text>
+                        <Card.Text>Article time</Card.Text>
+                    </Card.ImgOverlay>
                 </Card>
             </Col>
          </Row>
-
         </Container>
       </div>
   </Layout>
