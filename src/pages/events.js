6bca445450c2c270ac05e55a831fd65b4f6841de
Shain Dholakiya <shainshain99@gmail.com>
2020-10-05 22:52:10 -0400

    updates

--- src/pages/events.js
+++ src/pages/events.js
@@ -2,13 +2,29 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faInstagram, faTwitter, faYoutube} from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import {
+  faInstagram,
+  faTwitter,
+  faYoutube,
+} from "@fortawesome/free-brands-svg-icons"
 import "../components/layout.css"
-import { Row, Col, Container } from "react-bootstrap";
+import { Row, Col, Container } from "react-bootstrap"
 
-const monthNames = ["January", "February", "March", "April", "May", "June",
-  "July", "August", "September", "October", "November", "December"];
+const monthNames = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+]
 
 const date = new Date().getMonth()
 
@@ -16,89 +32,127 @@ const EventsPage = () => (
   <Layout>
     <SEO title="Calendar" />
     <div className="Hero Hero-Resources">
-        <Row>
-          <Col sm={{span: 8}} md={{span: 7}} lg={{span: 6}}>
-            <div className="Calendar">
-              <h1><u>{monthNames[date]}</u></h1>
+      <Row>
+        <Col sm={{ span: 8 }} md={{ span: 7 }} lg={{ span: 6 }}>
+          <div className="Calendar">
+            <h1>
+              <u>{monthNames[date]}</u>
+            </h1>
+          </div>
+        </Col>
+      </Row>
+      <Col
+        xs={{ span: 10, offset: 1 }}
+        sm={{ span: 10, offset: 1 }}
+        md={{ span: 10, offset: 1 }}
+        lg={{ span: 10, offset: 1 }}
+      >
+        <Row className="calendar-content">
+          <Col xs={12} sm={8} md={8} lg={6}>
+            <div>
+              <h2>Blockchain 101 Workshop</h2>
+              {/* <p>Pull up for some vibes and good food.</p> */}
+              <a target="_blank" href="">
+                Meeting Invitation
+              </a>
+            </div>
+          </Col>
+          <Col
+            xs={{ span: 12, offset: 0 }}
+            sm={{ span: 2, offset: 1 }}
+            md={{ span: 2, offset: 1 }}
+            lg={{ span: 2, offset: 2 }}
+          >
+            <div className="calendar-meeting-day">
+              <h2>Mon</h2>
+              <h2>12</h2>
             </div>
           </Col>
         </Row>
-          <Col xs={{span: 10, offset: 1}} sm={{span: 10, offset: 1}}  md={{span: 10, offset: 1}}  lg={{span: 10, offset: 1}} >
-              <Row className="calendar-content">
-                <Col xs={12} sm={8} md={8} lg={6}>
-                    <div>
-                        <h2>Blockchain 101 Workshop</h2>
-                        {/* <p>Pull up for some vibes and good food.</p> */}
-                        <a target="_blank" href="">Meeting Invitation</a>
-                    </div>
-                </Col>
-                <Col xs={{span: 12, offset: 0}} sm={{span: 2, offset: 1}} md={{span: 2, offset: 1}} lg={{span: 2, offset: 2}}>
-                    <div className="calendar-meeting-day">
-                        <h2>Mon</h2>
-                        <h2>12</h2>
-                    </div>
-                </Col>
-              </Row>
-              <Row className="calendar-content">
-                <Col xs={12} sm={8} md={8} lg={6}>
-                    <div>
-                        <h2>Blockchain of the Month</h2>
-                        {/* <p>Pull up for some vibes and good food.</p> */}
-                        <a target="_blank" href="">Meeting Invitation</a>
-                    </div>
-                </Col>
-                <Col xs={{span: 12, offset: 0}} sm={{span: 2, offset: 1}} md={{span: 2, offset: 1}} lg={{span: 2, offset: 2}}>
-                    <div className="calendar-meeting-day">
-                        <h2>Mon</h2>
-                        <h2>19</h2>
-                    </div>
-                </Col>
-              </Row>
-              <Row className="calendar-content">
-                <Col xs={12} sm={8} md={8} lg={6}>
-                    <div>
-                        <h2>Blockchain Use-Case Series</h2>
-                        {/* <p>Pull up for some vibes and good food.</p> */}
-                        <a target="_blank" href="">Meeting Invitation</a>
-                    </div>
-                </Col>
-                <Col xs={{span: 12, offset: 0}} sm={{span: 2, offset: 1}} md={{span: 2, offset: 1}} lg={{span: 2, offset: 2}}>
-                    <div className="calendar-meeting-day">
-                        <h2>Mon</h2>
-                        <h2>26</h2>
-                    </div>
-                </Col>
-              </Row>
-              <Row className="calendar-content">
-                <Col xs={12} sm={8} md={8} lg={6}>
-                    <div>
-                        {/* <h2>Blockchain Use-Case Series</h2>
-                        <a target="_blank" href="">Meeting Invitation</a> */}
-                    </div>
-                </Col>
-                <Col xs={{span: 12, offset: 0}} sm={{span: 2, offset: 1}} md={{span: 2, offset: 1}} lg={{span: 2, offset: 2}}>
-                    <div className="calendar-meeting-day">
-                        {/* <h2>Mon</h2>
-                        <h2>26</h2> */}
-                    </div>
-                </Col>
-              </Row>
-              <Row className="calendar-content">
-                <Col xs={12} sm={8} md={8} lg={6}>
-                    <div>
-                        {/* <h2>Blockchain Use-Case Series</h2>
-                        <a target="_blank" href="">Meeting Invitation</a> */}
-                    </div>
-                </Col>
-                <Col xs={{span: 12, offset: 0}} sm={{span: 2, offset: 1}} md={{span: 2, offset: 1}} lg={{span: 2, offset: 2}}>
-                    <div className="calendar-meeting-day">
-                        {/* <h2>Mon</h2>
-                        <h2>26</h2> */}
-                    </div>
-                </Col>
-              </Row>
+        <Row className="calendar-content">
+          <Col xs={12} sm={8} md={8} lg={6}>
+            <div>
+              <h2>Blockchain of the Month</h2>
+              {/* <p>Pull up for some vibes and good food.</p> */}
+              <a target="_blank" href="">
+                Meeting Invitation
+              </a>
+            </div>
           </Col>
-      </div>
+          <Col
+            xs={{ span: 12, offset: 0 }}
+            sm={{ span: 2, offset: 1 }}
+            md={{ span: 2, offset: 1 }}
+            lg={{ span: 2, offset: 2 }}
+          >
+            <div className="calendar-meeting-day">
+              <h2>Mon</h2>
+              <h2>19</h2>
+            </div>
+          </Col>
+        </Row>
+        <Row className="calendar-content">
+          <Col xs={12} sm={8} md={8} lg={6}>
+            <div>
+              <h2>Blockchain Use-Case Series</h2>
+              {/* <p>Pull up for some vibes and good food.</p> */}
+              <a target="_blank" href="">
+                Meeting Invitation
+              </a>
+            </div>
+          </Col>
+          <Col
+            xs={{ span: 12, offset: 0 }}
+            sm={{ span: 2, offset: 1 }}
+            md={{ span: 2, offset: 1 }}
+            lg={{ span: 2, offset: 2 }}
+          >
+            <div className="calendar-meeting-day">
+              <h2>Mon</h2>
+              <h2>26</h2>
+            </div>
+          </Col>
+        </Row>
+        <Row className="calendar-content">
+          <Col xs={12} sm={8} md={8} lg={6}>
+            <div>
+              {/* <h2>Blockchain Use-Case Series</h2>
+                        <a target="_blank" href="">Meeting Invitation</a> */}
+            </div>
+          </Col>
+          <Col
+            xs={{ span: 12, offset: 0 }}
+            sm={{ span: 2, offset: 1 }}
+            md={{ span: 2, offset: 1 }}
+            lg={{ span: 2, offset: 2 }}
+          >
+            <div className="calendar-meeting-day">
+              {/* <h2>Mon</h2>
+                        <h2>26</h2> */}
+            </div>
+          </Col>
+        </Row>
+        <Row className="calendar-content">
+          <Col xs={12} sm={8} md={8} lg={6}>
+            <div>
+              {/* <h2>Blockchain Use-Case Series</h2>
+                        <a target="_blank" href="">Meeting Invitation</a> */}
+            </div>
+          </Col>
+          <Col
+            xs={{ span: 12, offset: 0 }}
+            sm={{ span: 2, offset: 1 }}
+            md={{ span: 2, offset: 1 }}
+            lg={{ span: 2, offset: 2 }}
+          >
+            <div className="calendar-meeting-day">
+              {/* <h2>Mon</h2>
+                        <h2>26</h2> */}
+            </div>
+          </Col>
+        </Row>
+      </Col>
+    </div>
   </Layout>
 )
 
--- src/pages/resources.js
+++ src/pages/resources.js
@@ -4,7 +4,7 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import "../components/layout.css"
-import { Row, Col, Card, Container } from "react-bootstrap";
+import { Row, Col, Card, Container } from "react-bootstrap"
 
 import hackernoon from "../images/hackernoon-icon.png"
 import medium from "../images/medium-icon.png"
@@ -16,63 +16,68 @@ import discord from "../images/discord-icon.png"
 const ResourcesPage = () => (
   <Layout>
     <SEO title="Resources" />
-      <div className="Hero Hero-Resources">
+    <div className="Hero Hero-Resources">
+      <Row>
+        <Col
+          sm={{ span: 8, ofsset: 4 }}
+          md={{ span: 7, offset: 5 }}
+          lg={{ span: 6, offset: 6 }}
+        >
+          <div className="Resources">
+            <h1>Blockchain Resources</h1>
+          </div>
+        </Col>
+      </Row>
+      <Container>
         <Row>
-          <Col sm={{span: 8, ofsset: 4}} md={{span: 7, offset: 5}} lg={{span: 6, offset: 6}}>
-            <div className="Resources">
-              <h1>Blockchain Resources</h1>
-            </div>
+          <Col xs={12} sm={6} md={6} lg={4}>
+            <a target="_blank" href="https://discord.com/invite/EQnAG3D">
+              <Card className="resource-icons">
+                <Card.Img variant="top" src={discord} />
+              </Card>
+            </a>
+          </Col>
+          <Col xs={12} sm={6} md={6} lg={4}>
+            <a target="_blank" href="https://hackernoon.com/">
+              <Card className="resource-icons">
+                <Card.Img variant="top" src={hackernoon} />
+              </Card>
+            </a>
+          </Col>
+          <Col xs={12} sm={6} md={6} lg={4}>
+            <a target="_blank" href="https://medium.com/">
+              <Card className="resource-icons">
+                <Card.Img variant="top" src={medium} />
+              </Card>
+            </a>
+          </Col>
+          <Col xs={12} sm={6} md={6} lg={4}>
+            <a
+              target="_blank"
+              href="https://www.youtube.com/channel/UCUqBF-dLJDR2oe0GeicKVnA"
+            >
+              <Card className="resource-icons">
+                <Card.Img variant="top" src={youtube} />
+              </Card>
+            </a>
+          </Col>
+          <Col xs={12} sm={6} md={6} lg={4}>
+            <a target="_blank" href="https://www.reddit.com/">
+              <Card className="resource-icons">
+                <Card.Img variant="top" src={reddit} />
+              </Card>
+            </a>
+          </Col>
+          <Col xs={12} sm={6} md={6} lg={4}>
+            <a target="_blank" href="https://www.coindesk.com/">
+              <Card className="resource-icons">
+                <Card.Img variant="top" src={coindesk} />
+              </Card>
+            </a>
           </Col>
         </Row>
-        <Container>
-          <Row>
-            <Col xs={12} sm={6} md={6} lg={4}>
-                <a target="_blank" href="https://hackernoon.com/">
-                    <Card className="resource-icons">
-                        <Card.Img variant="top" src={hackernoon} />
-                    </Card>
-                </a>
-            </Col>
-            <Col xs={12} sm={6} md={6} lg={4}>
-                <a target="_blank" href="https://medium.com/">
-                    <Card className="resource-icons">
-                        <Card.Img variant="top" src={medium} />
-                    </Card>
-                </a>
-            </Col>
-            <Col xs={12} sm={6} md={6} lg={4}>
-                <a target="_blank" href="https://www.youtube.com/channel/UCUqBF-dLJDR2oe0GeicKVnA">
-                    <Card className="resource-icons">
-                        <Card.Img variant="top" src={youtube} />
-                    </Card>
-                </a>
-            </Col>
-            {/* </Row>
-            <Row> */}
-            <Col xs={12} sm={6} md={6} lg={4}>
-                <a target="_blank" href="https://www.reddit.com/">
-                    <Card className="resource-icons">
-                        <Card.Img variant="top" src={reddit} />
-                    </Card>
-                </a>
-            </Col>
-            <Col xs={12} sm={6} md={6} lg={4}>
-                <a target="_blank" href="https://www.coindesk.com/">
-                    <Card className="resource-icons">
-                        <Card.Img variant="top" src={coindesk} />
-                    </Card>
-                </a>
-            </Col>
-            <Col xs={12} sm={6} md={6} lg={4}>
-                <a target="_blank" href="https://discord.com/invite/EQnAG3D">
-                    <Card className="resource-icons">
-                        <Card.Img variant="top" src={discord} />
-                    </Card>
-                </a>
-            </Col>
-          </Row>
-        </Container>
-      </div>
+      </Container>
+    </div>
   </Layout>
 )
 
