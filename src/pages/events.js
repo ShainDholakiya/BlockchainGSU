d64f687ce8765fb85fc0e57066ecadf101a023d2
Shain Dholakiya <shainshain99@gmail.com>
2021-01-05 21:16:44 -0500

    updated about us page

--- src/pages/events.js
+++ src/pages/events.js
@@ -22,6 +22,61 @@ const monthNames = [
 
 const date = new Date().getMonth()
 
+let test = "<script type='text/javascript'>\
+$('#calendar').fullCalendar({\
+  header: {\
+    left: 'prev,next today',\
+    center: 'addEventButton',\
+    right: 'month,agendaWeek,agendaDay,listWeek'\
+  },\
+  defaultDate: '2018-11-16',\
+  navLinks: true,\
+  editable: true,\
+  eventLimit: true,\
+  events: [{\
+      title: 'Simple static event',\
+      start: '2018-11-16',\
+      description: 'Super cool event'\
+    },\
+\
+  ],\
+  customButtons: {\
+    addEventButton: {\
+      text: 'Add new event',\
+      click: function () {\
+        var dateStr = prompt('Enter date in YYYY-MM-DD format');\
+        var date = moment(dateStr);\
+\
+        if (date.isValid()) {\
+          $('#calendar').fullCalendar('renderEvent', {\
+            title: 'Dynamic event',\
+            start: date,\
+            allDay: true\
+          });\
+        } else {\
+          alert('Invalid Date');\
+        }\
+\
+      }\
+    }\
+  },\
+  dayClick: function (date, jsEvent, view) {\
+    var date = moment(date);\
+\
+    if (date.isValid()) {\
+      $('#calendar').fullCalendar('renderEvent', {\
+        title: 'Dynamic event from date click',\
+        start: date,\
+        allDay: true\
+      });\
+    } else {\
+      alert('Invalid');\
+    }\
+  },\
+});\
+\
+</script><div id='calendar'></div>"
+
 const EventsPage = () => (
   <Layout>
     <SEO title="Calendar" />
@@ -156,6 +211,10 @@ const EventsPage = () => (
         
       </Col>
     </div>
+
+    <div
+            dangerouslySetInnerHTML={{ __html: test }}
+          />
   </Layout>
 )
 
